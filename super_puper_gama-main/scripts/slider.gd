878e987fa03b9b31d951cbc7a62572e036816dc2
Perevertaylov Vasiliy <144835495+kum0o0@users.noreply.github.com>
2026-04-10 17:47:17 +0200

    Add files via upload

---
+++ super_puper_gama-main/scripts/slider.gd
@@ -0,0 +1,73 @@
+extends Node2D
+
+signal slider_hit(slider_node)
+signal slider_miss(slider_node)
+
+var ANGLE_ERROR: float = 0.5
+
+var checkpoints: PackedVector2Array = []
+var creation_time: int
+var current_time: int
+var lifetime: int
+
+var hit_radius: int = 50
+var curr_point_index = 0
+var states: Array = ["not active", "active", "finished"]
+var state: int = 0
+
+func _ready() -> void:
+	creation_time = Time.get_ticks_msec()
+	for i in checkpoints.size():
+		var tile = Sprite2D.new()
+		tile.texture = preload("res://src/sprites/note.png")
+		tile.position = checkpoints[i]
+		tile.scale = Vector2(0.5, 0.5)
+		add_child(tile)
+
+
+func _process(delta: float) -> void:
+	current_time = Time.get_ticks_msec()
+	if (current_time - creation_time) > lifetime:
+		miss()
+	if state == 0:
+		if current_time >= creation_time and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			var mouse_pos = get_global_mouse_position()
+			if mouse_pos.distance_to(checkpoints[0]) < hit_radius:
+				activate()
+	elif state == 1:
+		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			miss()
+			return
+		var mouse_pos = get_global_mouse_position()
+		if mouse_pos.distance_to(checkpoints[curr_point_index]) < hit_radius:
+			curr_point_index += 1
+			if curr_point_index >= checkpoints.size():
+				hit()
+		'''else:
+			var perf_vec: Vector2 = checkpoints[curr_point_index] - checkpoints[curr_point_index - 1]
+			var user_vec: Vector2 = mouse_pos - checkpoints[curr_point_index - 1]
+			if abs(perf_vec.angle() - user_vec.angle()) > ANGLE_ERROR:
+				miss()'''
+			
+
+func activate():
+	state = 1
+	curr_point_index = 1
+		
+		
+func hit():
+	state = 2
+	slider_hit.emit(self)
+	queue_free()
+	
+	
+func miss():
+	state = 2
+	slider_miss.emit(self)
+	queue_free()
+
+
+func get_end_position() -> Vector2:
+	if checkpoints.size() > 0:
+		return checkpoints[-1]
+	return global_position
